048ccb0c1a40c7518718e9026b0ce9620eeecca5
Sergio Escrivá <sergioescrivap@gmail.com>
2024-07-24 12:51:59 +0200

    Iniciacio SettleController

--- src/main/java/dev/sergioescriva/wallet/controllers/SettleController.java
+++ src/main/java/dev/sergioescriva/wallet/controllers/SettleController.java
@@ -1,10 +1,17 @@
 package dev.sergioescriva.wallet.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/settlecontroller")
+@RequestMapping("/settle")
 public class SettleController {
 
+    @GetMapping("/{wallet_id}")
+    public void getDivisionByWalletId(@PathVariable Long walletId) {
+        // settle = Settle().divisionWallet(wallet_id)
+        // return settle
+    }
 }
